4e47ab4d0239abc5dd322bf6ad9aeb43f2c2eb16
Ernesto Tagwerker <ernesto@etagwerker.com>
2012-10-07 23:13:51 -0300

    Added wysihtml5 editor.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,4 +7,15 @@ $(document).ready(function() {
 	  .addClass('link external')
 	  .attr('target', '_blank');
 
+   $('textarea').wysihtml5({
+			"font-styles": true, //Font styling, e.g. h1, h2, etc. Default true
+			"emphasis": true, //Italics, bold, etc. Default true
+			"lists": true, //(Un)ordered lists, e.g. Bullets, Numbers. Default true
+			"html": false, //Button which allows you to edit the generated HTML. Default false
+			"link": true, //Button to insert a link. Default true
+			"image": true, //Button to insert an image. Default true,
+			"color": false //Button to change color of font  
+		});
+  
+
 });
