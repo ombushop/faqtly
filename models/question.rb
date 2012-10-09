458e5199ddd7ed2797a63243f883fee984ad52b2
Ernesto Tagwerker <ernesto@etagwerker.com>
2012-10-08 21:55:52 -0300

    Added permalink to question model.

--- models/question.rb
+++ models/question.rb
@@ -1,16 +1,37 @@
+require 'pry'
+
 # encoding: utf-8
 class Question < Sequel::Model
   plugin :validation_helpers
-
+  
   def validate
     super
     validates_presence [:question, :answer]
     validates_unique [:question]
   end
 
+  # Finds a set of [Question] instances with the query.
+  # It searches through question and answer content.
+  # 
+  # @return [Array]
   def self.full_text_search(query)
     Question.where(Sequel.like(:answer, "%#{query}%")).
                   or(Sequel.like(:question, "%#{query}%"))
     # TODO full_text_search('answer', query)
+  end
+
+  # Finds a [Question] using the permalink
+  #
+  # @return [Question,nil]
+  def self.find_by_permalink(permalink)
+    Question.where(permalink: permalink).first
+  end
+
+  private
+
+  # Updates the permalink using Rack::Utils
+  def before_validation
+    binding.pry
+    self.permalink = Rack::Utils.escape(self.question)
   end
 end
