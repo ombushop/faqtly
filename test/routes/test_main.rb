5da189bba853a78c48bf48c361b58f4ff8d6cc67
Ernesto Tagwerker <ernesto@etagwerker.com>
2012-10-12 17:48:56 -0300

    Corrected test to take into account authorization.;

--- test/routes/test_main.rb
+++ test/routes/test_main.rb
@@ -16,7 +16,13 @@ class TestMain < Test::Unit::TestCase
     assert_equal 200, last_response.status
   end
 
-  def test_questions_new
+  def test_questions_new_for_unauthorized_user
+    get '/questions/new'
+    assert_equal 401, last_response.status
+  end
+
+  def test_questions_new_for_authorized_user
+    basic_authorize('admin', 'admin')
     get '/questions/new'
     assert last_response.body.include?("id='new-questions-form'")
   end
@@ -31,11 +37,13 @@ class TestMain < Test::Unit::TestCase
   end
 
   def test_create_a_question
+    basic_authorize('admin', 'admin')
     post '/questions', question: { question: "Hello?", answer: 'Hello world!' }
     assert_equal 302, last_response.status
   end
 
   def test_error_messages_on_failed_question_create
+    basic_authorize('admin', 'admin')
     old_count = Question.count
     post '/questions', question: { question: "", answer: "" }
     assert last_response.body.include?("answer is not present")
@@ -44,9 +52,11 @@ class TestMain < Test::Unit::TestCase
   end
 
   def test_questions_update
+
     @question = Question.create( question: 'Lightning?',
                       answer:   'Thunder!' )
 
+    basic_authorize('admin', 'admin')
     put "/questions/#{@question.id}", question: { question: "WWSJD?" }
     assert_equal "WWSJD?", Question[@question.id].question
   end
