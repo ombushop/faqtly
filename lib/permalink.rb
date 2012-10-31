08e0bbb50a829ae0dd9148cb8e854e3f0f970bfa
Ernesto Tagwerker <ernesto@etagwerker.com>
2012-10-31 18:13:34 -0300

    Made permalink def.

---
+++ lib/permalink.rb
@@ -0,0 +1,9 @@
+module Permalink
+
+  # Converts a string in a URL friendly string
+  # @param [String] Unescaped input
+  # @return [String] Escaped URL
+  def escape_for_url(string)
+    Rack::Utils.escape(string).gsub(/\+/,'-')
+  end
+end
