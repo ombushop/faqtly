f84c3bde800c22a54f03f506a5521c5f8c472bd1
Ernesto Tagwerker <ernesto@etagwerker.com>
2012-11-13 18:17:44 -0300

    Added tags helper.

--- helpers/init.rb
+++ helpers/init.rb
@@ -1,11 +1,11 @@
 # encoding: utf-8
 require_relative 'render_partial'
 require_relative 'authentication'
+require_relative 'tags_helper'
 require_relative 'questions_helper'
-
-
 
 Faqtly.helpers Rack::Utils
 Faqtly.helpers Authentication
 Faqtly.helpers RenderPartial
+Faqtly.helpers TagsHelper
 Faqtly.helpers QuestionsHelper
--- helpers/questions_helper.rb
+++ helpers/questions_helper.rb
@@ -1,6 +1,12 @@
 module QuestionsHelper
+
+  # Finds the right [Question] or raises a [Sinatra::NotFound] exception.
+  #
+  # @param [String] Permalink
+  # @param [Question]
+  # @raise [Sinatra::NotFound]
   def find_question(permalink)
-    result = Question.find_by_permalink(generate_permalink(permalink))
+    result = Question.find_by_permalink(permalink)
     raise Sinatra::NotFound unless result
     
     result
