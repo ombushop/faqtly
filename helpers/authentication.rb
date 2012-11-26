8e124d0999194f30d81659ccbcda66f0949d394b
Ernesto Tagwerker <ernesto@ombushop.com>
2012-11-26 16:43:40 -0300

    Added current_user to session .

--- helpers/authentication.rb
+++ helpers/authentication.rb
@@ -4,10 +4,27 @@ module Authentication
   #
   # Throws HTTP 401 when not authorized
   def protected!
-    unless authorized?
+    if authorized?
+      session[:user] = @auth.credentials.first
+    else
       response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
       throw(:halt, [401, "Not authorized\n"])
     end
+  end
+
+  # Returns the current user name from the session
+  #
+  # @return [String,nil]
+  def current_user
+    session[:user]
+  end
+
+  # Returns true or nil if the current user can or
+  # cannot do something
+  # 
+  # @return [String,nil]
+  def can?
+    current_user
   end
 
   # Determines if a user is authorized. It uses HTTP Authentication.
