677bb9853989fbb382c11b12cace5b131972c496
Ernesto Tagwerker <ernesto@ombushop.com>
2012-11-26 22:53:00 -0300

    Added more helper methods. ;

--- helpers/render_partial.rb
+++ helpers/render_partial.rb
@@ -4,7 +4,7 @@
 module RenderPartial
 
   def partial(page, options={})
-    haml page, options.merge!(:layout => false)
+    haml page, options.merge!(:layout => false), options[:locals]
   end
 
   # Calculates the page title using a question.
@@ -16,4 +16,37 @@ module RenderPartial
     title += question.question.to_s if question
     title
   end
+
+  # Returns the previous and next links if necessary. 
+  # 
+  # It uses `request.path`; params[:page] and opts[:scope]
+  # to determine the result.
+  #
+  # @return [String] HTML with previous, next links
+  def pagination(opts = {})
+    result = ""
+    scope = opts[:scope] || Question
+
+    if (size = scope.page_count) > 1
+      page = (params[:page] || 1).to_i
+
+      if page > 1
+
+        result += partial :'shared/_previous', locals: {page: page}
+
+        if page != size
+          result += partial :'shared/_next', locals: {page: page}
+        end
+      else
+        if size > 1
+          result += partial :'shared/_next', locals: {page: page}
+        end
+      end
+
+      result = "<div class='pagination'>#{result}</div>"
+    end
+
+    result
+  end
+
 end
